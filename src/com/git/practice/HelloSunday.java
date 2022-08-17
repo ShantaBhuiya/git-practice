c62e712bb7298f3d12679f7ab7bfbb1e3459498a
Shanta <shantatech02@gmail.com>
2022-08-17 12:39:08 -0400

    this is my 1st commit

---
+++ src/com/git/practice/HelloSunday.java
@@ -0,0 +1,10 @@
+package com.git.practice;
+
+public class HelloSunday {
+    public static void main(String[] args) {
+        System.out.println(helloJavaStudents());
+    }
+    public static String helloJavaStudents(){
+        return "How are you! How was your course learning";
+    }
+}
